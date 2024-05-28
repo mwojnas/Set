e9e78d3b3a6b12f4e8ce86aaddb9abd4a24c4b9b
mwojnas <matthias.woj@gmail.com>
2024-05-28 17:12:16 -0400

    first commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Matt Wojnas on 2024-05-28.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
